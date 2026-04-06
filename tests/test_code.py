d3531310051881d9cc10a4f06b08b856f75ff022
zanyvium <vzn123@live.dk>
2026-04-06 11:42:27 +0200

    C-TDD 2 - test: two lower case non-null names

--- tests/test_code.py
+++ tests/test_code.py
@@ -14,3 +14,8 @@ def test_stand_in_for_null():
 def test_shouting():
     name = "JERRY"
     assert greetings(name) == f"HELLO {name}!"
+
+
+def test_two_lower_case_names():
+    names = ["Jill", "Jane"]
+    assert greetings(names) == f"Hello {names[0]} and {names[1]}."
