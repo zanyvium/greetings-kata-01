5ae669b8fd73143da6bdf07671304f02cb3000c6
zanyvium <vzn123@live.dk>
2026-04-06 11:39:07 +0200

    C-TDD 3 - feat: shouting single

--- src/kata/code.py
+++ src/kata/code.py
@@ -1,5 +1,8 @@
 def greetings(name: str | None):
     if name:
-        return f"Hello, {name}."
+        if name.isupper():
+            return f"HELLO {name}!"
+        else:
+            return f"Hello, {name}."
     else:
         return "Hello, my friend."
