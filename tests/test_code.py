71d676f04e145c1e5d994d35953c66698b126a5f
zanyvium <vzn123@live.dk>
2026-04-06 11:13:25 +0200

    C-TDD 2 - test: stand in for nulls

--- tests/test_code.py
+++ tests/test_code.py
@@ -4,3 +4,8 @@ from src.kata.code import greetings
 def test_single_greeting():
     name = "Bob"
     assert greetings(name) == f"Hello, {name}."
+
+
+def test_stand_in_for_null():
+    name = ""
+    assert greetings(name) == "Hello, my friend."
